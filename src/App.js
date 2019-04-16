0ef1758d049fa2e21a329ed80f2cf63ff228321f
Divya Nair <divnairs@gmail.com>
2019-04-16 10:45:17 -0700

    sketch checkout page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react'
 
 import Login from './Login'
 import Signup from './Signup'
+import CheckoutTourist from './cart/CheckoutTourist'
+import CheckoutGuide from './cart/CheckoutGuide'
 
 function App() {
   return (
     <>
       <Login />
       <Signup />
+      <CheckoutTourist />
+      <CheckoutGuide />
     </>
   )
 }
