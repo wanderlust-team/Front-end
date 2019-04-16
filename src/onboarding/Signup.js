929cfaf2fbb2de2e804b731bd58632b182210edf
Divya Nair <divnairs@gmail.com>
2019-04-16 13:39:48 -0700

    style signup form

--- src/onboarding/Signup.js
+++ src/onboarding/Signup.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import styled from 'styled-components'
 
 function Signup() {
   const [username, setUsername] = useState('')
@@ -28,52 +29,89 @@ function Signup() {
   }
 
   return (
-    <>
-      <p>Sign Up</p>
+    <Form onSubmit={submitSignup}>
+      <label>
+        Username
+        <input
+          type="text"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+          required
+        />
+      </label>
 
-      <form onSubmit={submitSignup}>
-        <label>
-          Username
-          <input
-            type="text"
-            value={username}
-            onChange={e => setUsername(e.target.value)}
-            required
-          />
-        </label>
+      <label>
+        Password
+        <input
+          type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          required
+        />
+      </label>
 
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            required
-          />
-        </label>
+      <label>
+        E-mail
+        <input
+          type="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          required
+        />
+      </label>
 
-        <label>
-          E-mail
-          <input
-            type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-            required
-          />
-        </label>
+      <label>
+        You are a
+        <select onChange={e => setRole(e.target.value)} required>
+          <option value="tourist">tourist</option>
+          <option value="guide">guide</option>
+        </select>
+      </label>
 
-        <label>
-          You are a
-          <select onChange={e => setRole(e.target.value)} required>
-            <option value="tourist">tourist</option>
-            <option value="guide">guide</option>
-          </select>
-        </label>
-
-        <button>Create Account</button>
-      </form>
-    </>
+      <button>Create Account</button>
+    </Form>
   )
 }
 
 export default Signup
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  height: 800px;
+  width: 400px;
+  margin: auto;
+
+  input {
+    width: 380px;
+    height: 50px;
+    margin: 10px 0;
+    padding: 0 10px;
+    box-sizing: border-box;
+    font-size: 18px;
+    outline: none;
+  }
+
+  select {
+    background: transparent;
+    height: 30px;
+    width: 100px;
+    margin: 10px;
+    cursor: pointer;
+    outline: none;
+  }
+
+  button {
+    height: 50px;
+    width: 380px;
+    margin: 10px 0;
+    outline: none;
+    font-size: 18px;
+    font-weight: 500;
+    color: white;
+    background-color: mediumseagreen;
+    cursor: pointer;
+  }
+`
