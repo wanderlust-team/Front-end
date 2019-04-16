03add38b0ff2d4e07e6f5b4d37d288c72b7fbf16
Divya Nair <divnairs@gmail.com>
2019-04-16 13:09:28 -0700

    style login page

--- src/onboarding/Login.js
+++ src/onboarding/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import styled from 'styled-components'
 
 function Login() {
   const [username, setUsername] = useState('')
@@ -24,10 +25,8 @@ function Login() {
   }
 
   return (
-    <>
-      <p>Log In</p>
-
-      <form onSubmit={submitLogin}>
+    <FormContainer>
+      <Form onSubmit={submitLogin}>
         <label>
           Username
           <input
@@ -49,9 +48,54 @@ function Login() {
         </label>
 
         <button>Log In</button>
-      </form>
-    </>
+      </Form>
+
+      <p>Not yet registered? Sign-up</p>
+    </FormContainer>
   )
 }
 
 export default Login
+
+const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  height: 800px;
+  width: 400px;
+  margin: auto;
+
+  p {
+    text-align: center;
+  }
+`
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+
+  input {
+    width: 380px;
+    height: 50px;
+    margin: 10px 0;
+    padding: 0 10px;
+    box-sizing: border-box;
+    font-size: 18px;
+    outline: none;
+  }
+
+  button {
+    height: 50px;
+    width: 380px;
+    margin: 10px 0;
+    outline: none;
+    font-size: 18px;
+    font-weight: 500;
+    color: white;
+    background-color: mediumseagreen;
+    cursor: pointer;
+  }
+`
