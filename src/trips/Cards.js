fef0c1f3cb384242af43b7fe631738ac8f011f74
Divya Nair <divnairs@gmail.com>
2019-04-17 12:31:06 -0700

    add minimal trip cards style

--- src/trips/Cards.js
+++ src/trips/Cards.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import styled from 'styled-components'
 
 import Navigation from '../navigation/Navigation'
 
@@ -29,14 +30,43 @@ function Cards(props) {
   return (
     <>
       <Navigation {...props} />
-      {trips.map(trip => (
-        <div key={trip.id}>
-          <p>{trip.tripName}</p>
-          <p>{trip.location}</p>
-        </div>
-      ))}
+      <CardsContainer>
+        {trips.map(trip => (
+          <Card key={trip.id}>
+            <Name>{trip.tripName}</Name>
+            <Location>{trip.location}</Location>
+          </Card>
+        ))}
+      </CardsContainer>
     </>
   )
 }
 
 export default Cards
+
+const CardsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  width: 800px;
+  margin: auto;
+`
+
+const Card = styled.div`
+  border: 1px solid gainsboro;
+  height: 200px;
+  width: 200px;
+  margin: 10px;
+  padding: 5px;
+  box-sizing: border-box;
+  text-align: center;
+`
+
+const Name = styled.p`
+  font-weight: 600;
+`
+
+const Location = styled.p`
+  color: slategray;
+`
